5ae6f73aa65ec10d2fe868e519f30bde95b5b2cc
Patrícia Silva <pathy4491@gmail.com>
2025-07-22 23:41:16 -0400

    Atualizar o Conexao.cs

--- Intima Store/ConexaoBD.cs
+++ Intima Store/ConexaoBD.cs
@@ -5,18 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 
+
 namespace Intima_Store
 {
     public static class ConexaoBD
     {
-        private static string _connectionString =
-        "Server=localhost3306;Database=pdv_Uid=root;Pwd=Info@2025";
-         
+        private static string _connectionString = 
+            "Server=localhost;Port=3306;Database=pdv;Uid=root;Pwd=Info@2025";
+        
         private static MySqlConnection _connection;
 
-        public static string ConnectionString;
-        
-          
+        public static MySqlConnection GetConnection()
+        {
+            if (_connection == null)
+                _connection = new MySqlConnection(_connectionString);
+
+            return _connection;
+        }
+    }
+}
         
         
         
