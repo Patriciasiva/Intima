e98fa78d47fd92e445545878c3f65c6a631e6b77
2024.4.12 <2024.4.12@DESKTOP-GN0OUCS>
2025-07-22 21:55:03 -0400

    Add project files.

---
+++ Intima Store/ConexaoBD.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Intima_Store
+{
+    public static class ConexaoBD
+    {
+        private static string _connectionString =
+        "Server=localhost3306;Database=pdv_Uid=root;Pwd=Info@2025";
+         
+        private static MySqlConnection _connection;
+
+        public static string ConnectionString;
+        
+          
+        
+        
+        
+    }
+} 
+     
+
+    
+
---
+++ Intima Store/Produtos/ProdutoFeminino.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intima_Store.Produtos
+{
+    public class ProdutoFeminino
+    {
+        public ProdutoFeminino BuscarPorCodigoBarras(string codigo)
+        {
+            using(var conexao = ConexaoBD.GetConexao())
+            {
+                conexao.Open();
+                var cmd = new MySqlCommand("SELECT FROM produtos WHERE codigo_barras=@codigo", conexao);
+                cmd.Parameters.AddWithValue("@codigo",codigo);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if(reader.Read())
+                    {
+                        return new ProdutoFeminino();
+                    }
+                }
+            }
+        }
+    }
+}
